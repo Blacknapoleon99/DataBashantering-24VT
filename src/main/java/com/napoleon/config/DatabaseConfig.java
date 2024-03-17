ff18c8bd7568665cbb9623fc4a1170ee7332f0bd
Kevin Lübeck <127541113+Blacknapoleon99@users.noreply.github.com>
2024-03-17 18:23:09 +0100

    Update DatabaseConfig.java

--- src/main/java/com/napoleon/config/DatabaseConfig.java
+++ src/main/java/com/napoleon/config/DatabaseConfig.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig {
-    private static final String URL = "jdbc:mysql://awsmobiledb.c9cuyeyqqn8m.eu-north-1.rds.amazonaws.com:3306/aws-mobile_db";
+    private static final String URL = "";
     private static final String USER = "aws_admin";
-    private static final String PASSWORD = "c1q6QDf4osfmBnXeZ6pL";
+    private static final String PASSWORD = "";
 
     public static String getUrl() {
         return URL;
